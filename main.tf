ebf621f4b2df84bb7cbe24221fedfbdd4bdb3ad4
Ashish maurya <76896126+Ashishm0511@users.noreply.github.com>
2025-01-09 16:53:48 +0530

    Commited main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
 
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "mi-01816d07b1128cd2d"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
