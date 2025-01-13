c389b2205a969acc087963ffd971500759b25eea
Ashish <ashishm1900@gmail.com>
2025-01-13 23:49:38 +0530

    new

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "mi-01816d07b1128cd2d"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
-    instance_
+    instance_type = "t3.micro"
 
